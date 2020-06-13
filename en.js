d69bfc4812c054c92d1337180bef8d2b93dd2835
hhilalkaya <66560268+hhilalkaya@users.noreply.github.com>
2020-06-13 20:34:02 +0300

    en.js

---
+++ en.js
@@ -0,0 +1,42 @@
+// Toolbox
+const _toolbox_title = "Toolbox for Artificial Intelligence";
+const _tool_exp = " <p>Artificial intelligence is a research area which aims to produce machines, \n" +
+    " that think and feel like a human, by using man-made materials. Including computer technologies for production and development, \n" +
+    "it is located at the intersection of many branches of science.  Currently, we are quite far from the development of\n" +
+    "an artificial intelligence that thinks and feels like a human being. But with the development of  machine learning techniques,\n" +
+    "a sub-branch of artificial intelligence, the production of different type of products\n" +
+    " that facilitate human life and understand us better has become very easy.\n" +
+    "</p>\n" +
+    "<p class='p_link'><a class='more' href='https://teachablemachine.withgoogle.com/'>👀 We can do a quick experiment with Teachable Machine to quickly see how artificial intelligence works </a></p>\n";
+const _tool_code = "<h2>👩‍💻 Coding Environment </h2>\n" +
+    "<p class='exp_text'>\n" +
+    " Artificial intelligence can be classified as a sub-branch of computer science. To communicate with computers,\n" +
+    " we use programming languages. Let's make some drawings to get acquainted with coding\n" +
+    "or get used to the programming environment we will use in the activities we will complete.\n" +
+    "</p>\n" +
+    "<p class='p_link'><a class='more' href='turtle/index.html'>🐢 Drawing with Turtle  </a></p>";
+const _tool_vision = "<h2>📷 How Computers See?</h2>\n" +
+    "<p class='exp_text'>Computer vision covers the algorithms used in all kinds of imaging technology, from phones in our pocket to medical devices. \n" +
+    "    Every algorithm, from scans which understand the damage that has occurred in our brains \n" +
+    "     when we hit our heads to Instagram filters, enters the field of computerized vision.\n" +
+    "</p>\n" +
+    "<p class='p_link'><a class='more' href='vision/eye/index.html'>👀 How do our eyes work? How do we perceive light and colors? </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/color/color.html'>🌈 What is a pixel? How do we represent colors on the computer? </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/color/colorwheel.html'>🍭 How do we easily find color definitions?  </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/color_quiz/index.html'>🎆 Try to find the right definitions of colors! </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/color/colorschemes.html'>🏵️ Why do we need different color presentations?</a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/grayscale/grayscale.html'>👵 What are the benefits of turning a photo from colorful to gray shades?</a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/binarization/binarization.html'>🦓 What is a bitmap? How to create a picture with 0's and 1's?  </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/chromakey/index.html'>🌈 Chroma Key Algorithm </a></p>\n" +
+    "<p class='p_link'><a class='more' href='vision/filter/index.html'>🖼️ What is a filter? What is its contribution to artificial intelligence algorithms? </a></p>\n" +
+    "<p class='p_link'><a class='more' href='https://transcranial.github.io/keras-js/#/mnist-cnn'>🕸️ What do neural networks see in pictures? </a></p>";
+const _tool_music = "<h2>🎼 Music Knowledge</h2>\n" +
+    "<p class='exp_text'>\n" +
+    "Before moving on to how computers hear, let's repeat our basic knowledge of music and the relationship of music with physics. So let's start with a little music programming exercise."\n" +
+    "</p>\n" +
+    "<p class='p_link'><a class='more' href='sound/music/index.html'>🎹 Music Programming </a></p>";
+const _tool_speech = "<h2>👂 How Computers Hear? </h2>\n" +
+    "<p class='p_link'><a class='more' href='sound/rounded_bars/index.html'>🔉 How Loudness Looks on the Computer? </a></p>\n" +
+    "<p class='p_link'><a class='more' href='sound/fft_freq_spectrum/index.html'>📈 What does the frequency analysis of the sounds we hear look like? </a></p>";
+// Helper Methods
+
